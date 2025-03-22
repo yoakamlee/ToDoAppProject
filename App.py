b81de2102de9746907f8ff3316cfa325dd399d44
yoakamlee <yoakamleesoupen@gmail.com>
2025-03-22 20:32:58 +0200

    Added new case "show" - yls

--- App.py
+++ App.py
@@ -1,16 +1,19 @@
-todos = ["Monster hunter", "tekken", "Path Of exile"]
+todos = ["Monster hunter", "Tekken", "Path Of Exile"]
 
 todos.sort()
 
 while True:
     user_input = input("Select an option: add, show, edit, complete, or exit: ")
-    user_input.strip()
+    user_input.strip().lower()
 
     match user_input:
         case "add":
-            new_todo = input("Add a todo: ")
+            new_todo = input("Add a todo: ").title()
             print(f"New Todo added: {new_todo}")
             todos.append(new_todo)
+
+        case "show":
+            print(todos)
 
         case "exit":
             break
