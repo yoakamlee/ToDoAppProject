aa10b6edbc4600a494f24ef0c9964f0bcbd6e73c
yoakamlee <yoakamleesoupen@gmail.com>
2025-03-27 20:23:26 +0200

    Added with-context to file reading and writing for better code optimisation, changed some variable names to make more sense. Added file editing and deleting items from a file  - yls

--- App.py
+++ App.py
@@ -1,47 +1,61 @@
 
 while True:
     user_input = input("Select an option: add, show, edit, complete, or exit: ")
-    user_input.strip().lower()
+    user_input = user_input.strip().lower()
 
     match user_input:
         case "add":
-            new_todo = input("Add a todo: ").title() + "\n"
-            print(f"New Todo added: {new_todo}")
+            added_todo = input("Add a todo: ").title() + "\n"
+            print(f"New Todo added: {added_todo}")
 
-            file = open("todos.txt", "r")
-            todos = file.readlines()
-            file.close()
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
 
-            todos.append(new_todo)
-            file = open("todos.txt", "w")
-            file.writelines(todos)
-            file.close()
+            todos.append(added_todo)
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
 
         case "show":
 
-            file = open("todos.txt", "r")
-            todos = file.readlines()
-            file.close()
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
 
-            new_todos = [item.strip("\n").title() for item in todos]
+            edited_todos = [item.strip("\n").title() for item in todos]
 
-            for index,item in enumerate(new_todos):
+            for index,item in enumerate(edited_todos):
                 index = index + 1
                 print(f"{index}: {item}")
 
         case "edit":
             num = int(input("Enter Item Number: "))
             num = num - 1
-            print(f"Todo chosen: {todos[num]}")
-            todos[num] = input("Enter Todo: ").title()
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
+            print(f"Todo chosen: {todos[num].strip("\n")}")
+            todos[num] = input("Enter Todo: ").title() + "\n"
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
 
         case "complete":
             num = int(input("Enter Item Number: "))
             num = num - 1
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
             print(f"Item selected is: {todos[num]}")
             todos.pop(num)
+
+            with open("todos.txt","w") as file:
+                file.writelines(todos)
+
             for item in todos:
-                print(item)
+                item = item.title().strip("\n")
+                print(f"{item}")
 
         case "exit":
             break
