57defcf26b66ad56c403f0d0f2600f523752424b
yoakamlee <yoakamleesoupen@gmail.com>
2025-03-31 21:35:46 +0200

    Added elif statements to better handle user inputs and so the user can add, edit, complete or show the item(s) within the same input the code now checks the entire string for relevant inputs at the same time.  - yls

--- App.py
+++ App.py
@@ -1,63 +1,64 @@
-
 while True:
     user_input = input("Select an option: add, show, edit, complete, or exit: ")
     user_input = user_input.strip().lower()
 
-    match user_input:
-        case "add":
-            added_todo = input("Add a todo: ").title() + "\n"
-            print(f"New Todo added: {added_todo}")
+    if "add" in user_input:
+        added_todo = user_input[4:]
+        print(f"New Todo added: {added_todo}")
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            todos.append(added_todo)
+        todos.append(added_todo)
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-        case "show":
+    elif "show" in user_input:
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            striped_todos = [item.strip("\n").title() for item in todos]
+        striped_todos = [item.strip("\n").title() for item in todos]
 
-            for index,item in enumerate(striped_todos):
-                index = index + 1
-                print(f"{index}: {item}")
+        for index,item in enumerate(striped_todos):
+            index = index + 1
+            print(f"{index}: {item}")
 
-        case "edit":
-            num = int(input("Enter Item Number: "))
-            num = num - 1
+    elif "edit" in user_input:
+        num = int(user_input[5:])
+        num = num - 1
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            print(f"Todo chosen: {todos[num].strip("\n")}")
-            todos[num] = input("Enter Todo: ").title() + "\n"
+        print(f"Todo chosen: {todos[num].strip("\n")}")
+        todos[num] = input("Enter Todo: ").title() + "\n"
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-        case "complete":
-            num = int(input("Enter Item Number: "))
-            num = num - 1
+    elif "complete" in user_input:
+        num = int(user_input[9:])
+        num = num - 1
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            print(f"Item selected is: {todos[num]}")
-            todos.pop(num)
+        print(f"Item selected is: {todos[num]}")
+        todos.pop(num)
 
-            with open("todos.txt","w") as file:
-                file.writelines(todos)
+        with open("todos.txt","w") as file:
+            file.writelines(todos)
 
-            for item in todos:
-                item = item.title().strip("\n")
-                print(f"{item}")
+        for item in todos:
+            item = item.title().strip("\n")
+            print(f"{item}")
 
-        case "exit":
-            break
+    elif "exit" in user_input:
+        break
+
+    else:
+        print(f"{user_input} unknown command!")
 
 print("Todo app exited")
