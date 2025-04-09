cb3eeeb1be76560796ac03de530da2d2f15ed842
yoakamlee <yoakamleesoupen@gmail.com>
2025-04-09 19:53:04 +0200

    added default arguments to get_todos() and write_todos(), also added doc strings as descriptions. - yls

--- App.py
+++ App.py
@@ -1,10 +1,14 @@
-def get_todos(filepath):
+def get_todos(filepath="todos.txt"):
+    """Reads a text file from the filepath passed to it"""
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_args):
+def write_todos(todos_args, filepath="todos.txt"):
+    """Writes to a text file from the file path passed
+    to it, also takes in a list of items
+    """
     with open(filepath, "w") as file_local:
         file_local.writelines(todos_args)
 
@@ -17,15 +21,15 @@ while True:
         added_todo = user_input[4:]
         print(f"New Todo added: {added_todo}")
 
-        todos = get_todos("todos.txt")
+        todos = get_todos()
 
         todos.append(added_todo + "\n")
 
-        write_todos("todos.txt", todos)
+        write_todos(todos)
 
     elif user_input.startswith("show"):
 
-        todos = get_todos("todos.txt")
+        todos = get_todos()
 
         striped_todos = [item.strip("\n").title() for item in todos]
 
@@ -38,12 +42,12 @@ while True:
             num = int(user_input[5:])
             num = num - 1
 
-            todos = get_todos("todos.txt")
+            todos = get_todos()
 
             print(f"Todo chosen: {todos[num].strip("\n")}")
             todos[num] = input("Enter Todo: ").title() + "\n"
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
 
         except ValueError:
             print("Unknown Command! Invalid Value")
@@ -58,12 +62,12 @@ while True:
             num = int(user_input[9:])
             num = num - 1
 
-            todos = get_todos("todos.txt")
+            todos = get_todos()
 
             print(f"Item selected is: {todos[num]}")
             todos.pop(num)
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
 
             for item in todos:
                 item = item.title().strip("\n")
